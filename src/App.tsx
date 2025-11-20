dd0de8056a753f32ca212d262b69eded1d885bf0
carsten-l <clu@rts.dk>
2025-11-20 14:26:53 +0100

    refactor: remove App.css and update App component structure with theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import ThemeSwitcher from './components/ThemeSwitcher.js';
+import useThemeStore from './stores/ThemeStore.js';
+
+useThemeStore.subscribe((state) => {
+  document.documentElement.setAttribute('data-theme', state.theme);
+});
+
+document.documentElement.setAttribute('data-theme', useThemeStore.getState().theme);
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <ThemeSwitcher />
+      <header className='mb-8'>
+        <div className="container mx-auto flex justify-between items-center py-6 px-8">
+          <div className='text-left'>
+          <p className='text-xl font-black'>GAL</p>
+          <p className='text-xs'>Guardian Article Lister</p>
+          </div>
+        <ThemeSwitcher />
+
+        </div>
+      </header>
+      <main>
+        <div className="container mx-auto py-6 px-8">
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -30,6 +39,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+          </div>
+
+      </main>
+      
     </>
   )
 }
