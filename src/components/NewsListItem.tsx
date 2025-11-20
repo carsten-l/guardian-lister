2fd684b3fc12c779ca7ec75c0ebca4a74e4f35a0
carsten-l <clu@rts.dk>
2025-11-20 15:16:43 +0100

    add NewsListItem component to display individual news articles

---
+++ src/components/NewsListItem.tsx
@@ -0,0 +1,15 @@
+import { type NewsItem } from "../lib/types.js";
+
+export default function NewsListItem({ item }: { item: NewsItem }) {
+  return (
+    <li key={item.id} className="mb-4 border p-4 space-y-2">
+      <p className="text-xs font-black text-gray-500 dark:text-gray-300 tracking-widest uppercase">{item.sectionName}</p>
+      <h2 className="text-balance">{item.webTitle}</h2>
+      <p className="text-sm">{ new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "2-digit"
+      } ).format(new Date(item.webPublicationDate))}</p>
+    </li>
+  );
+}
