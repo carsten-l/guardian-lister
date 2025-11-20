7a227869404586bafc01b60cb0d2f14e3c1e48ca
clurts <clu@rts.dk>
2025-11-20 18:30:40 +0100

    edited text

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -8,11 +8,10 @@ function RouteComponent() {
     return (
         <div className="space-y-4">
             <h1 className="text-2xl font-bold">About</h1>
-            <p>Hello "/about"!</p>
             <p>This is the about page of the Guardian Article Lister.</p>
             <p>
                 The routes sole purpose is to have somwhere to navigate to test
-                staletime on the loader...
+                staletime on the loader on the index page...
             </p>
         </div>
     );
