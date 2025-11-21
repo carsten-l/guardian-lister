ca611d1ab39e670440c4a115cba6ee6a2ef295c7
carsten-l <clu@rts.dk>
2025-11-21 13:18:15 +0100

    visual tweaks

--- src/components/NewsListItem.tsx
+++ src/components/NewsListItem.tsx
@@ -2,8 +2,13 @@ import { type NewsItem } from "../types";
 
 export default function NewsListItem({ item }: { item: NewsItem }) {
   return (
-    <li key={item.id} className="mb-4 border p-4 space-y-2">
-      <p className="text-xs font-black text-gray-500 dark:text-gray-300 tracking-widest uppercase">{item.sectionName}</p>
+    <li key={item.id} className="mb-4 border border-neutral-400 p-4 space-y-2">
+      <p className="text-xs font-bold text-neutral-400 dark:text-neutral-200 tracking-[2px] uppercase">
+        {item.pillarName !== item.sectionName 
+          ? `${item.pillarName} ● ${item.sectionName}` 
+          : item.sectionName
+        }
+      </p>
       <h2 className="text-balance">{item.webTitle}</h2>
       <p className="text-sm">{ new Intl.DateTimeFormat("en-US", {
           year: "numeric",
