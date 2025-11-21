84f351386ab141a780c34cfb607b69aefc6754cd
carsten-l <clu@rts.dk>
2025-11-21 15:01:15 +0100

    add loading and not found components

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,8 @@
+
+
+export default function NotFound() {
+  return <div>
+    <h1 className="text-2xl font-bold text-balance">404 - Page Not Found</h1>
+    <p className="mt-4">Sorry, the page you are looking for does not exist.</p>
+  </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import NotFound from './components/NotFound'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import Loading from './components/Loading'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ 
+  routeTree, 
+  defaultNotFoundComponent: NotFound,
+  defaultPendingComponent: Loading
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
