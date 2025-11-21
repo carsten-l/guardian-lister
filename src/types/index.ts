1c7e0023d97be292bad0ac4a0a59c21e108bb315
carsten-l <clu@rts.dk>
2025-11-21 13:17:16 +0100

    add api response type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,21 @@
 export type NewsItem = {
     id: string;
     webTitle: string;
+    pillarName: string;
     webPublicationDate: string;
     sectionName: string;
 };
+
+export type GuardianApiResponse = {
+    response: {
+        status: string;
+        userTier: string;
+        total: number;
+        startIndex: number;
+        pageSize: number;
+        currentPage: number;
+        pages: number;
+        orderBy: string;
+        results: NewsItem[];
+    };
+}
