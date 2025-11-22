5008e3de476e0dafce372496e749b6c6ab4ba52d
clurts <clu@rts.dk>
2025-11-22 16:53:45 +0100

    update icons

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,23 +1,21 @@
-import { WeatherSunny32Light, WeatherMoon32Light } from "@fluentui/react-icons";
+import { IoMoon, IoSunny } from "react-icons/io5";
 import useThemeStore from "../stores/ThemeStore";
 
 export default function ThemeSwitcher() {
+    const theme = useThemeStore((state) => state.theme);
+    const toggleTheme = useThemeStore((state) => state.toggleTheme);
 
-  const theme = useThemeStore((state) => state.theme);
-  const toggleTheme = useThemeStore((state) => state.toggleTheme);
-  
- return (
+    return (
         <button
             className={`p-2 rounded-full flex justify-center items-center ${theme === "light" ? "bg-gray-200" : "bg-gray-600"}`}
             onClick={toggleTheme}
             aria-label="Toggle Theme"
         >
             {theme === "light" ? (
-                <WeatherMoon32Light />
+                <IoMoon size="1.5em" />
             ) : (
-                <WeatherSunny32Light />
+                <IoSunny size="1.5em" />
             )}
         </button>
     );
-};
-
+}
